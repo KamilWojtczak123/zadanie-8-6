120f5d5b69ec21a0b1b8445addda5a2f90307c78
Kamil Wojtczak <kamil.wojtczak1@gmail.com>
2018-01-29 10:45:01 +0100

    Add index i js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// script js
+var a, b;
+value = (a * a) - (2 * a *b) +(b * b);
+console.log("Podaj wartosc a " + a +"Podaj wartosc b "+ b +"Wynik to: " + value);
+if (value > 0) {
+    console.log("Wynik dodatni");
+} else if(value < 0) {
+    console.log("Wynik ujemny");
+} else {
+    console.log("Wynik jest równy zero");
+}
